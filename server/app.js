182ffae7e1363ce1941a51ed36b49034e806eb6a
mayankbuwade <mayank.buade@gmail.com>
2023-08-28 12:43:06 +0530

    minor changes

--- server/app.js
+++ server/app.js
@@ -8,6 +8,8 @@ const cors = require("cors");
 
 //all routes
 const UserRoutes = require("./routes/userRoutes");
+const ProductsRoutes = require("./routes/productRoutes");
+const ReviewRoutes = require("./routes/reviewRoutes");
 
 //swagger yaml setup
 const file = fs.readFileSync("./swagger.yaml", "utf8");
@@ -33,6 +35,8 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //routes
 app.use("/api/v1", UserRoutes);
+app.use("/api/v1", ProductsRoutes);
+app.use("/api/v1", ReviewRoutes);
 
 //app export
 module.exports = app;
